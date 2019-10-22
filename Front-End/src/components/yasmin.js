e439e55d733ad16321c504b8b7ec0783af04f335
yasmin-alharasis <yasmin.jaalharasis@gmail.com>
2019-10-22 19:57:48 +0300

    second commit

--- Front-End/src/components/yasmin.js
+++ Front-End/src/components/yasmin.js
@@ -1,9 +1,77 @@
+/*
+
 import React from 'react';
 import { photos } from "./photos";
 import Gallery from "react-photo-gallery";
+// popout the browser and maximize to see more rows! ->
+const BasicRows = () => (
+<Router>
+<Gallery photos={photos} onClick={(phot)=>{
+    console.log(phot)
+    // <Link to=""></Link>
 
-/* popout the browser and maximize to see more rows! -> */
-const BasicRows = () => <Gallery photos={photos} />;
+}} />
+</Router>
+)
+
 export default BasicRows;
+*/
 
+
+import React, { useState, useCallback } from "react";
+import { render } from "react-dom";
+import Gallery from "react-photo-gallery";
+import SelectedImage from "./s";
+import { photos } from "./photos";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+export default function BasicRows() {
+  const [selectAll, setSelectAll] = useState(false);
+
+  const toggleSelectAll = () => {
+    setSelectAll(!selectAll);
+  };
+
+  const imageRenderer = useCallback(
+    ({ index, left, top, key, photo }) => (
+      <SelectedImage
+        selected={selectAll ? true : false}
+        key={key}
+        margin={"2px"}
+        index={index}
+        photo={photo}
+        left={left}
+        top={top}
+      />
+    ),
+    [selectAll]
+  );
+
+  return (
+    <Router>
+
+    <div>
+      <p>
+        <button onClick={toggleSelectAll}>toggle select all</button>
+      </p>
+      <Switch>
+        <Route exact path="/">
+             <Gallery photos={photos} renderImage={imageRenderer} />
+         </Route>
+         <Route path="/photo/:src">
+            <h1>{window.location.pathname.slice(7)}</h1>
+         </Route>
+        </Switch>
+    </div>
+
+    </Router>
+  );
+}
+
+// render(<App />, document.getElementById("app"));
 
